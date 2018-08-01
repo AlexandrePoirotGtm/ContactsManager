74674efe3a96d1c158a44e1cf6f9d8672b2729ea
Hatem sat <Adminl@PARME22>
2018-08-01 16:02:52 +0200

    Perfection

--- ContactsManager/Program.cs
+++ ContactsManager/Program.cs
@@ -8,8 +8,76 @@ namespace ContactsManager
 {
     class Program
     {
+        static List<string> contact = new List<string>();
+
         static void Main(string[] args)
         {
+            string Choix;
+            do
+            {
+                AffichageMenu();
+                Choix = Console.ReadLine();
+                ChoixMenu(Choix,contact);
+                
+            } while(Choix!="4");
         }
+        static void AffichageMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Menu\n1. Liste des Contacts\n2. Ajout des contacts\n3. Suppresion d'un contact\n4. Quitter\nTapez votre choix, puis validez avec entrer:");
+        }
+        
+        static void ChoixMenu(string Choix,List<string> ListContact)
+        {
+            switch (Choix)
+            {
+                case "1":
+                    ListerContacts(ListContact);
+                    break;
+
+                case "2":
+                    AjouterContact(ListContact);
+                    break;
+
+                case "3":
+                    SupprimerContact(ListContact);
+                    break;
+
+                case "4":
+
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Mauvais choix");
+                    break;
+            }
+        }
+        static void AjouterContact(List<string> ListContact)
+        {
+            Console.Clear();
+            Console.WriteLine("Rentrez le nom du contact à ajouter");
+            ListContact.Add(Console.ReadLine());
+            Console.Clear();
+        }
+
+        static void ListerContacts(List<string> ListContact)
+        {
+            Console.Clear();
+            foreach (String LeContact in contact)
+            {
+                Console.WriteLine(LeContact);
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void SupprimerContact(List<string> ListContact)
+        {
+            Console.Clear();
+            Console.WriteLine("Rentrez le nom du contact à supprimer");
+            ListContact.Remove((Console.ReadLine()));
+            Console.Clear();
+        }
+
     }
 }
